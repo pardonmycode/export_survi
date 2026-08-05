164fcb076a136bf5d46496edea806eef518f4ab3
pardonmycode <106189370+pardonmycode@users.noreply.github.com>
2026-08-05 07:41:07 +0200

    Refactor NixOS installation script for clarity

--- nixos_install_qemu.sh
+++ nixos_install_qemu.sh
@@ -1,75 +1,108 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
 
-DISK="/dev/vda"
+echo "--------------------------------------------------------------------------------"
+echo "Your attached storage devices will now be listed."
+read -p "Press 'q' to exit the list. Press enter to continue." NULL
 
-echo "=== WARNING ==="
-echo "ALL DATA ON ${DISK} WILL BE DELETED!"
-read -rp "Type YES to continue: " ANSWER
+sudo fdisk -l | less
 
-[ "$ANSWER" = "YES" ] || exit 1
+echo "--------------------------------------------------------------------------------"
+echo "Detected the following devices:"
+echo
 
-echo "== Partitioning =="
-
-parted -s "${DISK}" \
-    mklabel msdos \
-    mkpart primary ext4 1MiB 100% \
-    set 1 boot on
-
-sleep 2
-
-echo "== Formatting =="
-
-mkfs.ext4 -F -L nixos "${DISK}1"
-
-echo "== Mounting =="
-
-mount "${DISK}1" /mnt
-
-echo "== Generating configuration =="
-
-nixos-generate-config --root /mnt
-
-cat >> /mnt/etc/nixos/configuration.nix <<'EOF'
-
-boot.loader.grub.enable = true;
-boot.loader.grub.device = "/dev/vda";
-
-networking.hostName = "nixos";
-
-services.openssh.enable = true;
-
-users.users.nixos = {
-  isNormalUser = true;
-  extraGroups = [ "wheel" ];
-  initialPassword = "nixos";
-};
-
-security.sudo.wheelNeedsPassword = false;
-
-environment.systemPackages = with pkgs; [
-  git
-  vim
-  curl
-  wget
-  htop
-];
-
-system.stateVersion = "25.05";
-EOF
+i=0
+for device in $(sudo fdisk -l | grep "^Disk /dev" | awk '{print $2}' | sed 's/://'); do
+    echo "[$i] $device"
+    i=$((i+1))
+    DEVICES[$i]=$device
+done
 
 echo
-echo "==============================="
-echo "Installing NixOS..."
-echo "==============================="
+read -p "Which device do you wish to install on? " DEVICE
 
-nixos-install
+DEV=${DEVICES[$(($DEVICE+1))]}
+
+read -p "How much swap space do you need in GiB (e.g. 8)? " SWAP
+
+read -p "Will now partition ${DEV} with swap size ${SWAP}GiB. Ok? Type 'go': " ANSWER
+
+if [ "$ANSWER" = "go" ]; then
+    echo "Partitioning ${DEV}..."
+
+    (
+      echo o          # New DOS partition table
+
+      echo n          # New partition
+      echo p          # Primary
+      echo 1          # Partition 1
+      echo            # Default start
+      echo -${SWAP}G  # Leave space for swap
+
+      echo n          # New partition
+      echo p          # Primary
+      echo 2          # Partition 2
+      echo            # Default start
+      echo            # Default end
+
+      echo t          # Change type
+      echo 2          # Partition 2
+      echo 82         # Linux swap
+
+      echo a          # Make bootable
+      echo 1          # Partition 1
+
+      echo p
+      echo w
+    ) | sudo fdisk ${DEV}
+
+else
+    echo "Cancelled."
+    exit
+fi
+
+echo "--------------------------------------------------------------------------------"
+echo "Getting created partition names..."
+
+P1=${DEV}1
+P2=${DEV}2
+
+echo "Root: ${P1}"
+echo "Swap: ${P2}"
+
+echo "--------------------------------------------------------------------------------"
+read -p "Press enter to install NixOS."
+
+sudo mkfs.ext4 -L nixos ${P1}
+
+sudo mkswap -L swap ${P2}
+sudo swapon ${P2}
+
+sudo mount ${P1} /mnt
+
+echo "Generating configuration..."
+
+sudo nixos-generate-config --root /mnt
 
 echo
-echo "Set root password:"
-passwd
+echo "Open configuration.nix"
+read -p "Press Enter..."
+
+sudo nano /mnt/etc/nixos/configuration.nix
 
 echo
-echo "Installation finished!"
+echo "IMPORTANT!"
+echo "Make sure configuration.nix contains:"
 echo
-echo "Shutdown the VM, remove the ISO and boot from the disk."
+echo 'boot.loader.grub.enable = true;'
+echo "boot.loader.grub.device = \"${DEV}\";"
+echo
+
+read -p "Press Enter to install..."
+
+sudo nixos-install
+
+echo
+echo "Installation complete."
+echo "Remove the ISO and reboot."
+
+reboot
